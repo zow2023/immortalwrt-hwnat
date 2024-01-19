c9c253c7b80f4a89489786a232fa596236799513
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-01-19 19:11:04 +0800

    Create diy-part2.sh

---
+++ ramips-hnat /diy-part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP 默认IP由0.1修改为1.1
+sed -i 's/192.168.0.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+#
