a544fd9bfe407f0c2c23b626a997f5736017213a
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-04-08 17:41:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,11 +23,11 @@ git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-a
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/luci/applications/luci-app-passwall
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-#git clone https://github.com/xiaorouji/openwrt-passwall.git -b  luci-smartdns-dev package/passwall/luci
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b  luci-smartdns-dev package/passwall/luci
 rm -rf feeds/package/helloworld
-#git clone https://github.com/fw876/helloworld.git -b master package/helloworld
+git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
