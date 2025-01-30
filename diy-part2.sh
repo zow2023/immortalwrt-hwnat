0351f9088dce5306333fb15009b94c27364b3913
zow2023 <125123077+zow2023@users.noreply.github.com>
2025-01-30 23:55:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,15 +11,15 @@
 #
 # sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 
-rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+#rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
 
 rm -rf feeds/luci/applications/luci-app-smartdns
 
 git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git -b lede feeds/luci/applications/luci-app-smartdns/
-
+git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
 # git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
-git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
+#git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 # rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -40,5 +40,5 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # Modify hostname
-#sed -i 's/ImmortalWrt/LINKSYS-WRT/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/R3G-WRT/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/LINKSYS-WRT/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/R3G-WRT/g' package/base-files/files/bin/config_generate
